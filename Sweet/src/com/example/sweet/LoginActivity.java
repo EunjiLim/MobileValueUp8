f189b09c23d5c8f674ed21c85333ad91c64434cd
임은지 <cucumbers816@gmail.com>
2015-07-14 04:32:30 +0900

    temporary sign-in frame and class

--- Sweet/src/com/example/sweet/LoginActivity.java
+++ Sweet/src/com/example/sweet/LoginActivity.java
@@ -21,6 +21,10 @@ public class LoginActivity extends ActionBarActivity {
 		startActivity(intent);
 	}
 
+	public void onSignIn(View v){
+		Intent intent = new Intent(this, SignInActivity.class);
+		startActivity(intent);
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
