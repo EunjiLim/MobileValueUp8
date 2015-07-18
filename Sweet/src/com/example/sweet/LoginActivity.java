fb1fc03be91205d1f51cf43170b4dc38755e0145
EunjiLim <cucumbers816@gmail.com>
2015-07-18 16:14:32 +0900

    commit for 영주

--- Sweet/src/com/example/sweet/LoginActivity.java
+++ Sweet/src/com/example/sweet/LoginActivity.java
@@ -1,26 +1,153 @@
 package com.example.sweet;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends ActionBarActivity {
 
+	EditText ID;
+	EditText PW;
+	Button login;
+	String id,pw;
+	String line="",temp="";
+	private logIn loging;
+	
+	//context for Toast.makeText
+	private Context context;
+	
+	//boolean variable for login
+	boolean loginFlag = false;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		startActivity(new Intent(this, SplashActivity.class));
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		ID = (EditText)findViewById(R.id.emailAddress);
+		PW = (EditText)findViewById(R.id.pw);
+		login = (Button)findViewById(R.id.button_login);
+		
+		context = this;
+		
+		login.setOnClickListener(new OnClickListener()   {		
+			public void onClick(View v)
+			{
+				 loging = new logIn();
+			     loging.execute();
+		
+			}	
+		} );
 	}
 	
+public class logIn extends AsyncTask<String, Void, String> {
+		
+	    protected void onPreExecute() {
+            super.onPreExecute();
+        }
+	    
+		protected String doInBackground(String... params) {
+	    		    	
+			 try {
+				URL myUrl = new URL("http://52.69.67.4/login.php");
+			    HttpURLConnection http = (HttpURLConnection) myUrl.openConnection();
+			    Log.e("tag","2");
+			    if(http!=null)
+			    {
+			    	 http.setUseCaches(false);                                            
+		             http.setDoInput(true);                         // 서버에서 읽기 모드 지정 
+		             http.setDoOutput(true);                       // 서버로 쓰기 모드 지정  
+		             http.setRequestMethod("POST");
+		             http.setRequestProperty("charset", "UTF-8") ;  
+		             
+                    // 연결되었음 코드가 리턴되면.
+		           
+                    id = ID.getText().toString();
+                    pw = PW.getText().toString();
+                    
+                    StringBuffer buffer = new StringBuffer(); 
+                    buffer.append("id").append("=").append(id).append("&");                 // php 변수에 값 대입 
+                    buffer.append("pw").append("=").append(pw); 
+                    
+                    OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream()); 
+                    PrintWriter writer = new PrintWriter(outStream); 
+                    writer.write(buffer.toString()); 
+                    writer.flush(); 
+                    writer.close();	
+                    outStream.close();
+                    
+                    BufferedReader br = new BufferedReader(new InputStreamReader(http.getInputStream(), "UTF-8"));
+
+                    for(;;){
+                    line = br.readLine(); 
+                    
+                    	if(line!=null){
+                    		Log.e("tag",line);
+                    		temp+=line;                    	
+                    	}
+                        if(line == null) break;
+                    }
+
+                    if(temp.equals("1"))
+                    {
+                    	loginFlag = true;
+                    	Log.e("tag","exi"); //toast,  페이지 전환
+                    } else
+                    {
+                    	Log.e("tag","no"); //toast ID 또는 PW워드가 틀립니다.
+                    } 
+                    temp="";
+                    loginFlag = false;
+                    br.close();          	
+			    }
+                    http.disconnect();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;      	    
+	    }
+
+	    protected void onPostExecute(String result) {   	
+            super.onPostExecute(result);
+            
+            //login 완료 되면 FragmentBaseActivity로 화면 전환
+			Intent intent = new Intent(context, FragmentBaseActivity.class);
+			startActivity(intent);
+        }	
+	}
+
 	public void onLoginWithoutId(View v){
 		Intent intent = new Intent(this, FragmentBaseActivity.class);
 		startActivity(intent);
 	}
 
+	public void onClickSignIn(View v){
+		Intent intent = new Intent(this, SignInActivity.class);
+		startActivity(intent);
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- Sweet/src/com/example/sweet/SignInActivity.java
+++ Sweet/src/com/example/sweet/SignInActivity.java
@@ -1,15 +1,220 @@
 package com.example.sweet;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignInActivity extends Activity{
 	
+	private MyAsyncTask myAsyncTask;
+	private checkup checking;
+	EditText ID, PW;
+	EditText NAME, BIRTHDAY, SEX, PHONE, FACEBOOK;
+	Button finish, duplicationCheck;
+	String id, pw, name, birthday, sex, phone, facebook;
+	String temp1 = "", temp2="";
+	String line = "";
+	
+	//context for toast.maketext
+	private Context context;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signin);
+		ID = (EditText)findViewById(R.id.EditText_id);
+		PW = (EditText)findViewById(R.id.EditText_password);
+		NAME = (EditText)findViewById(R.id.EditText_name);
+		BIRTHDAY = (EditText)findViewById(R.id.EditText_dateOfBirth);
+		SEX = (EditText)findViewById(R.id.EditText_sex);
+		PHONE = (EditText)findViewById(R.id.EditText_cellNumber);
+		FACEBOOK = (EditText)findViewById(R.id.EditText_facebookId);
+		finish = (Button)findViewById(R.id.Button_finishSignIn);
+		duplicationCheck = (Button)findViewById(R.id.Button_duplicationCheck);
+		
+		context = this;
+		
+		duplicationCheck.setOnClickListener(new OnClickListener() {
+			public void onClick(View v)
+			{
+				checking = new checkup();
+				checking.execute();
+			}
+		});
+		
+		finish.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				myAsyncTask = new MyAsyncTask();
+				myAsyncTask.execute();
+			}
+		});
+	}
+	
+	public class MyAsyncTask extends AsyncTask<String, Void, String> {
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+		
+		protected String doInBackground(String... params) {
+			try {
+				URL myUrl = new URL("http://52.69.67.4/signup.php");
+			    HttpURLConnection http = (HttpURLConnection) myUrl.openConnection();
+			
+			    if(http!=null)
+			    {				    	    				  			    
+			    	 http.setUseCaches(false);                                            
+		             http.setDoInput(true);                         // 서버에서 읽기 모드 지정 
+		             http.setDoOutput(true);                       // 서버로 쓰기 모드 지정  
+		             http.setRequestMethod("POST");
+		             http.setRequestProperty("charset", "UTF-8") ;  
+                  
+                    // 연결되었음 코드가 리턴되면.
+		           
+                    id = ID.getText().toString();
+                    pw = PW.getText().toString();
+                    name = NAME.getText().toString();
+                    birthday = BIRTHDAY.getText().toString();
+                    sex = SEX.getText().toString();
+                    phone = PHONE.getText().toString();
+                    facebook = FACEBOOK.getText().toString();
+                    
+                    //php변수에 값을 대입하고 buffer에 잇는 과정
+                    StringBuffer buffer = new StringBuffer(); 
+                    buffer.append("id") .append("=") .append(id).append("&");
+                    buffer.append("pw").append("=").append(pw).append("&");    
+                    buffer.append("name").append("=").append(name).append("&");
+                    buffer.append("birthday").append("=").append(birthday).append("&");
+                    buffer.append("sex").append("=").append(sex).append("&");
+                    buffer.append("phone").append("=").append(phone).append("&");
+                    buffer.append("facebook").append("=").append(facebook);
+                    Log.e("tag",buffer.toString());
+                    
+                    OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream()); 
+                    PrintWriter writer = new PrintWriter(outStream); 
+                    writer.write(buffer.toString()); 
+                    writer.flush(); 
+                    writer.close();	
+                    outStream.close();
+                    
+                    BufferedReader br = new BufferedReader(new InputStreamReader(http.getInputStream(), "UTF-8"));
+          
+                    
+                    for(;;){
+                     
+                    	String line = br.readLine();
+      
+                        temp1 += line;
+                        
+                        if(line == null) break;
+                    }
+                              
+                    br.close();
+			    }
+                    http.disconnect();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;       	    
+	    }
+		
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+	
+		}
 	}
 
+public class checkup extends AsyncTask<String, Void, String> {
+		
+	    protected void onPreExecute() {
+            super.onPreExecute();
+        }
+	    
+		protected String doInBackground(String... params) {	    		    	
+			 try {
+				URL myUrl = new URL("http://52.69.67.4/checking.php");
+			    HttpURLConnection http = (HttpURLConnection) myUrl.openConnection();
+			
+			    if(http!=null)
+			    {
+			    	 http.setUseCaches(false);                                            
+		             http.setDoInput(true);                         // 서버에서 읽기 모드 지정 
+		             http.setDoOutput(true);                       // 서버로 쓰기 모드 지정  
+		             http.setRequestMethod("POST");
+		             http.setRequestProperty("charset", "UTF-8") ;  
+		             
+                    // 연결되었음 코드가 리턴되면.
+		           
+                    id = ID.getText().toString();
+                    
+                    StringBuffer buffer = new StringBuffer(); 
+                    buffer.append("id").append("=").append(id).append("");                 // php 변수에 값 대입                
+                    
+                    OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream()); 
+                    PrintWriter writer = new PrintWriter(outStream); 
+                    writer.write(buffer.toString()); 
+                    writer.flush(); 
+                    writer.close();	
+                    outStream.close();
+                    
+                    BufferedReader br = new BufferedReader(new InputStreamReader(http.getInputStream(), "UTF-8"));
+                                                 
+                    for(;;){
+                    line = br.readLine(); 
+                    
+                    	if(line!=null){
+                    		Log.e("tag",line);
+                    		temp2+=line;                   	
+                    	}
+                        if(line == null) break;
+                    }                                                                                                                
+                    
+                    if(temp2.equals("1"))
+                    {
+                    	Toast toast = Toast.makeText(context , "이미 ID가 존재합니다.", Toast.LENGTH_SHORT); 
+                    	toast.show(); 
+                    	Log.e("tag","이미 ID가 존재합니다."); //toast 이미 ID가 존재합니다.                    	                                        
+                    } else                    
+                    {      
+                    	Toast toast = Toast.makeText(context , "사용 할 수 있는 ID입니다.", Toast.LENGTH_SHORT);
+                    	Log.e("tag","you can use the ID"); //toast 사용해도 좋다.
+                    }                                                        
+                    temp2="";                    
+                    br.close();                     	
+			    }
+                    http.disconnect();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;       	    
+	    }
+			    
+	    protected void onPostExecute(String result) {	    	
+            super.onPostExecute(result);                         
+        }	
+	}	
+	
 }
