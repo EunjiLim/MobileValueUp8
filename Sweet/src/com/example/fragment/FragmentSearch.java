10ddfef7b5f669ee582a8f867c282d466fb26767
EunjiLim <cucumbers816@gmail.com>
2015-07-27 01:07:47 +0900

    서버에서 게시판 받아오기.

--- Sweet/src/com/example/fragment/FragmentSearch.java
+++ Sweet/src/com/example/fragment/FragmentSearch.java
@@ -1,61 +1,187 @@
 package com.example.fragment;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import com.example.libriary.JSONParser;
 import com.example.listview.IconTextItem;
 import com.example.listview.IconTextListAdapter;
 import com.example.sweet.R;
+import com.example.sweet.SetGroupActivity;
 
-public class FragmentSearch extends Fragment{
-	
+public class FragmentSearch extends Fragment {
+
 	ListView listView1;
 	IconTextListAdapter adapter;
-	
+
+	// 데이터를 읽어올 때 사용할 변수
+	ArrayList<HashMap<String, String>> boardlist = new ArrayList<HashMap<String, String>>();
+
+	// 상세검색 버튼
+	Button descriptionButton;
+
+	// 게시판 리스트 불러올 url
+	private static String url = "http://52.69.67.4/make.php";
+
+	JSONArray boardArray = null;
+
+	// JSON Node Names
+	private static final String TAG_OS = "board";
+
+	private static final String TAG_ID = "ID";
+	private static final String TAG_TITLE = "title";
+	private static final String TAG_LOCATION = "location";
+	private static final String TAG_DATE = "date";
+	private static final String TAG_PEOPLE = "people";
+
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_search, container, false);
-        
-        listView1 = (ListView) v.findViewById(R.id.listView_searchFragment);
-        adapter = new IconTextListAdapter(getActivity());
-        
-        Resources res = getResources();
-        adapter.addItem(new IconTextItem(res.getDrawable(R.drawable.profileicon),"전주에서 노실분","1","전라도","2015-7-22","4"));
-        
-        //리스트뷰에 어댑더 설정
-        listView1.setAdapter(adapter);
-        
-        //새로 정의한 리스너로 객체를 만들어 설정
-        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_search, container, false);
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                IconTextItem curItem = (IconTextItem) adapter.getItem(position);
-                String[] curData = curItem.getData();
+		//리스트뷰 
+		listView1 = (ListView) v.findViewById(R.id.listView_searchFragment);
+		adapter = new IconTextListAdapter(getActivity());
 
-                Toast.makeText(getActivity().getApplicationContext(), "Selected : " + curData[0], Toast.LENGTH_LONG).show();
+		new JSONParse().execute();
+		return v;
+	}
 
-            }
-        });
-        
-        return v;
-    }
-    
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        super.onViewCreated(view, savedInstanceState);
-    }
-    
-    public void setData(String title, String description, String location, String date, int people){
-    	Toast.makeText(getActivity().getApplicationContext(), ""+title+people, Toast.LENGTH_SHORT).show();
-    }
+	private class JSONParse extends AsyncTask<String, String, JSONObject> {
+		private ProgressDialog pDialog;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(getActivity());
+			pDialog.setMessage("Getting Data ...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+
+		}
+
+		@Override
+		protected JSONObject doInBackground(String... args) {
+
+			Log.i("TAG", "#");
+			JSONParser jParser = new JSONParser();
+
+			// Getting JSON from URL
+			Log.i("TAG", "$");
+			JSONObject json = jParser.getJSONFromUrl(url);
+			Log.i("TAG", "%");
+			return json;
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject json) {
+			pDialog.dismiss();
+			try {
+				Log.i("TAG", "onPostExecute시작");
+
+				// Getting JSON Array from URL
+				boardArray = json.getJSONArray(TAG_OS);
+
+				for (int i = 0; i < boardArray.length(); i++) {
+					JSONObject c = boardArray.getJSONObject(i);
+
+					// Storing JSON item in a Variable
+					String id = c.getString(TAG_ID);
+					String title = c.getString(TAG_TITLE);
+					String location = c.getString(TAG_LOCATION);
+					String date = c.getString(TAG_DATE);
+					String people = c.getString(TAG_PEOPLE);
+					// Adding value HashMap key => value
+
+					/*
+					 * HashMap<String, String> map = new HashMap<String,
+					 * String>();
+					 * 
+					 * map.put(TAG_ID, id); map.put(TAG_TITLE, title);
+					 * map.put(TAG_DATE, date); map.put(TAG_PEOPLE, people);
+					 */
+
+					/*
+					 * Log.i("TAG", "*"); boardlist.add(map);
+					 */
+					/*
+					 * adapter = new SimpleAdapter(getActivity(), boardlist,
+					 * R.layout.list_v, new String[] { TAG_VER,TAG_NAME, TAG_API
+					 * }, new int[] { R.id.vers,R.id.name, R.id.api});
+					 */
+
+					Resources res = getResources();
+
+					Log.i("TAG", "*");
+					adapter.addItem(new IconTextItem(res
+							.getDrawable(R.drawable.profileicon), title, "1",
+							location, date, people));
+					listView1.setAdapter(adapter);
+					Log.i("TAG", "(");
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onViewCreated(view, savedInstanceState);
+		
+		//리스트 뷰 항목 클릭했을 때
+		listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Toast.makeText(
+						getActivity().getApplicationContext(),
+						"You Clicked",0).show();
+
+			}
+		});
+		
+		//상세 검색 버튼 눌렀을 때
+		descriptionButton = (Button) view
+				.findViewById(R.id.Button_searchDetailed);
+		descriptionButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			}
+		});
+	}
+
+	public void setData(String title, String description, String location,
+			String date, int people) {
+		Toast.makeText(getActivity().getApplicationContext(),
+				"" + title + people, Toast.LENGTH_SHORT).show();
+	}
 }
---
+++ Sweet/src/com/example/libriary/JSONParser.java
@@ -0,0 +1,101 @@
+package com.example.libriary;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+ 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+import android.util.Log;
+ 
+/*****************************************
+ * 
+ * URL로부터 JSON 데이터를 받고 JSON Object를 return
+ * 해주는 JSONParser class를 생성
+ * [] 은 JSON형식에서 배열인데 이것도 객체로 받을 수 있는지가 의문임.
+ */
+public class JSONParser {
+	 
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
+ 
+    // constructor
+    public JSONParser() {
+ 
+    }
+ 
+    public JSONObject getJSONFromUrl(String url) {
+ 
+        // Making HTTP request
+        try {
+        	Log.i("TAG","JSONObject 1");
+            // defaultHttpClient
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            
+        	Log.i("TAG","JSONObject 2");
+            HttpPost httpPost = new HttpPost(url);
+            
+        	Log.i("TAG","JSONObject 3");
+ 
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            
+        	Log.i("TAG","JSONObject 4");
+            HttpEntity httpEntity = httpResponse.getEntity();
+            
+        	Log.i("TAG","JSONObject 5");
+            is = httpEntity.getContent();
+ 
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+        try {
+        	Log.i("TAG","JSONObject 6");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "utf-8"), 8);
+            
+        	Log.i("TAG","JSONObject 7");
+            StringBuilder sb = new StringBuilder();
+        	Log.i("TAG","JSONObject 8");
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        	Log.i("TAG","JSONObject 9");
+            is.close();
+            json = sb.toString();
+            //////////////////////
+            String firstS = "{\"board\":";
+            json = firstS + json + "}";
+            
+            Log.i("TAG", json);
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+ 
+        // try parse the string to a JSON object
+        try {
+        	Log.i("TAG","JSONObject 10");
+            jObj = new JSONObject(json);
+        	Log.i("TAG","JSONObject 11");
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+    	Log.i("TAG","JSONObject 12");
+        // return JSON String
+        return jObj;
+    }
+}
